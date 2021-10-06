27adc90499610420b4099262c1ddf06e1d74ce91
Bernardo <bernardopissutti@gmail.com>
2021-10-05 23:57:54 -0300

    Início dos testes unitarios

---
+++ Cadastro-Loja/src/testesJUnit/TestesDeletar.java
@@ -0,0 +1,17 @@
+package testesJUnit;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import control.*;
+
+public class TestesDeletar {
+	
+	ControlDados dado = new ControlDados();
+//	 
+//	@Test
+//	void testDeletarCliente() {
+//		assertTrue(dado.deletarCliente(4));
+//	}
+}
